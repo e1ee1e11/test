99660a1ae29f5f7c4007cd678aa4329bb3cc9eab
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2014-07-07 14:38:03 +0800

    del a.out

--- hello1.c
+++ hello1.c
@@ -3,17 +3,10 @@ main()
 {
 	printf("hello world\n");
 	printf("My name is e1ee1e11\n");
-<<<<<<< HEAD
 	/*
-		wwwwwwwwww
-		xxxxxxxxxx
-		yyyyyyyyyy
 		zzzzzzzzzz
 	*/
         /*test test*/
-=======
 	/*Thannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnk U*/
-	//????????
->>>>>>> third
 }
 
