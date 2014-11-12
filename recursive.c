2ee0236221a4bf0d5236d6e85681f14168d84d50
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2014-11-12 14:27:40 +0800

    complete file and recursive test

--- recursive.c
+++ recursive.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
+int sum(int n);
+
 void main(void)
 {
-	FILE *fp;
-	int n;
-	fp = fopen("input.txt", "r");
-	fscanf(fp, "%d",&n);
-	printf("%d\n",n);
+	FILE *fp1, *fp2;
+	int n, N;
+	fp1 = fopen("input.txt", "r");
+	fscanf(fp1, "%d",&n);
+	fclose(fp1);
+	N = sum(n);
+	fp2 = fopen("output.txt", "w");
+	fprintf(fp2, "%d\n", N);
+
+}
+
+int sum(int n)
+{
+	if(n==1)
+	{
+		return n;
+	}
+	else
+	{
+		return (n + sum(n-1));
+	}
+	
 }
