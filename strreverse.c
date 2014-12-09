aba04b2c5406fc59d77e3d82c4ab4d45fb160ef1
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2014-12-09 17:35:02 +0800

    string reverse

---
+++ strreverse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int strreserve(char *str)
+{
+	int i;
+	for(i=strlen(str)-1;i>=0;i--)
+	printf("%c",str[i]);
+	printf("\n");
+}
+
+int main()
+{	
+	char str[20];
+	gets(str);
+	strreserve(str);
+	return 0;
+}
