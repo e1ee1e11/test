10a3ce69eb29d2b024448bf1f2262273fafb9121
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2014-12-10 21:55:37 +0800

    wire min compare wtih 3 integer

--- defineMin.c
+++ defineMin.c
@@ -2,10 +2,13 @@
 
 #define Min(A,B) ((A)<=(B)?(A):(B))
 #define Max(A,B) (A>=B?(A):(B))
-
+int min(int a,int b,int c)
+{
+	return (a<=b && a<=c)?a:min(b,c,a);
+}
 int main()
 {	
 	printf("%d, %d\n",Min(5,4),Max(5,4));
-	
+	printf("MIN=%d\n",min(9,8,7));
 	return 0;
 }
