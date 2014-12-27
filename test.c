174fac3b398ad2d1401a1c7209ab4d4f0886fe79
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2014-12-27 13:21:36 +0800

    easy pointer

--- test.c
+++ test.c
@@ -1,45 +1,12 @@
 #include <stdio.h>
 
-main()
+void main(void)
 {
-	char c = 'a' ;
-	char b = 110 ;
-	char *a = &c ;
-	char *d = &b ;
-	printf("a=%p, *a=%d\n", a, *a);
-	printf("b=%p, b=%d\n", b, b);
-	printf("c=%p, c=%d\n", c, c);
-	printf("d=%p, *d=%d\n", d, *d);
-	printf("-----------------------------------------------------1\n");
-	*a = b;
-	printf("a=%p, *a=%d\n", a, *a);
-        printf("b=%p, b=%d\n", b, b);
-        printf("c=%p, c=%d\n", c, c);
-        printf("d=%p, *d=%d\n", d, *d);
-	printf("-----------------------------------------------------2\n");
-	a++;
-	printf("a=%p, *a=%d\n", a, *a);
-        printf("b=%p, b=%d\n", b, b);
-        printf("c=%p, c=%d\n", c, c);
-        printf("d=%p, *d=%d\n", d, *d);
-        printf("-----------------------------------------------------3\n");
-	a = &b;
-	printf("a=%p, *a=%d\n", a, *a);
-        printf("b=%p, b=%d\n", b, b);
-        printf("c=%p, c=%d\n", c, c);
-        printf("d=%p, *d=%d\n", d, *d);
-        printf("-----------------------------------------------------4\n");
-	b--;
-	printf("a=%p, *a=%d\n", a, *a);
-        printf("b=%p, b=%d\n", b, b);
-        printf("c=%p, c=%d\n", c, c);
-        printf("d=%p, *d=%d\n", d, *d);
-        printf("-----------------------------------------------------5\n");
-	*d = 110;
-	printf("a=%p, *a=%d\n", a, *a);
-        printf("b=%p, b=%d\n", b, b);
-        printf("c=%p, c=%d\n", c, c);
-        printf("d=%p, *d=%d\n", d, *d);
-        printf("-----------------------------------------------------6\n");
-	//printf("*a=%c, b=%c, c=%c, d=%c\n", *a, b, c, *d);
+	int (*a)(int);
+	int *ptr;
+	int n=5;
+	ptr = &n;
+	n=6;
+	printf("%d\n",*ptr+1);
+	printf("%d\n", n);
 }
