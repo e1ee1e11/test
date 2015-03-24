f44ac797f5a4cd365c92e2af17b97a6508942326
e1ee1e11 <e1ee1e11e1ee@gmail.com>
2015-03-24 18:06:35 +0800

    add comments

--- argc_argv.c
+++ argc_argv.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+/*****************
+
+$ ./a.out hello world
+
+argc=3
+argv[0]=./a.out
+argv[1]=hello
+argv[2]=world
+
+*******************/
 
 int main(int argc, char *argv[])
 {
